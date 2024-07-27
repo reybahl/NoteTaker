5ffa3fa77da025032f3e42104b35e46ac96d3684
reybahl <bahl.reyansh@gmail.com>
2024-07-27 15:39:00 -0400

    add material angular, navabr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoursesComponent } from './courses.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CoursesComponent],
+  imports: [RouterOutlet, CoursesComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
